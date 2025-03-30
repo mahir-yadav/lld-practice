2a80812b8595d1a169c5405625d61361b6008fe7
mahir <mahir21263@iiitd.ac.in>
2025-03-31 03:47:59 +0530

    post and show questions and answers

--- stack-overflow/main.cpp
+++ stack-overflow/main.cpp
@@ -20,6 +20,7 @@ using namespace std;
 
 // Stackoverflow
 //
+class Answer;
 class User
 {
 private:
@@ -33,6 +34,10 @@ public:
         this->id = id;
         this->reputation = 0;
     }
+    string getName()
+    {
+        return name;
+    }
 };
 class Question
 {
@@ -40,11 +45,39 @@ private:
     int id;
     string title, content;
     User *author;
-    vector<Comment *> comments;
+    // vector<Comment *> comments;
     vector<Answer *> answers;
-    vector<Tag *> tags;
-    vector<Vote *> votes;
+    // vector<Tag *> tags;
+    // vector<Vote *> votes;
+
 public:
+    Question(int id, User *author, string title, string content)
+    {
+        this->id = id;
+        this->author = author;
+        this->title = title;
+        this->content = content;
+    }
+    User *getAuthor()
+    {
+        return author;
+    }
+    string getTitle()
+    {
+        return title;
+    }
+    int getId()
+    {
+        return id;
+    }
+    vector<Answer *> getAnswers()
+    {
+        return answers;
+    }
+    void addAnswer(Answer *answer)
+    {
+        answers.push_back(answer);
+    }
 };
 class Answer
 {
@@ -53,31 +86,121 @@ private:
     string content;
     User *author;
     Question *question;
-    vector<Comment *> comments;
-    vector<int> votes;
+    // vector<Comment *> comments;
+    // vector<int> votes;
+
 public:
+    Answer(int id, string content, User *author, Question *question)
+    {
+        this->id = id;
+        this->content = content;
+        this->author = author;
+        this->question = question;
+    }
+    int getId()
+    {
+        return id;
+    }
+    User *getAuthor()
+    {
+        return author;
+    }
+    string getContent()
+    {
+        return content;
+    }
 };
-class Comment
+// class Comment
+// {
+// private:
+//     int id;
+//     string content;
+//     User *author;
+//     Question *question;
+//     Answer *answer;
+
+// public:
+// };
+// class Vote
+// {
+// private:
+//     User *voter;
+//     bool isUpvote;
+
+// public:
+// };
+// class Tag
+// {
+// private:
+//     int id;
+//     string name;
+
+// public:
+// };
+class Stackoverflow
 {
 private:
-    int id;
-    string content;
-    User *author;
-    Question *question;
-    Answer *answer;
+    vector<Question *> questions;
+
 public:
+    void postQuestion(User *author, string title, string content)
+    {
+        questions.push_back(new Question(questions.size() + 1, author, title, content));
+        cout << "Question posted: " << title << " by " << author->getName() << endl;
+    }
+    void showAllQuestions()
+    {
+        for (auto &question : questions)
+        {
+            cout << question->getId() << ". " << question->getTitle() << " by " << question->getAuthor()->getName() << endl;
+        }
+    }
+    void postAnswer(User *author, int questionId, string content)
+    {
+        for (auto &question : questions)
+        {
+            if (question->getId() == questionId)
+            {
+                Answer *newAns = new Answer(question->getAnswers().size() + 1, content, author, question);
+                question->addAnswer(newAns);
+
+                cout << "Answer Posted to question: " << question->getTitle() << " by " << author->getName() << endl;
+                return;
+            }
+        }
+        cout << "No Question found with id: " << questionId << endl;
+    }
+    void showAnswers(int questionId)
+    {
+        for (auto &question : questions)
+        {
+            if (question->getId() == questionId)
+            {
+                vector<Answer *> ans = question->getAnswers();
+                if (ans.size() == 0)
+                {
+                    cout << "No answer for this question yet!" << endl;
+                    return;
+                }
+                for (auto &answer : ans)
+                {
+                    cout << answer->getId() << ". " << answer->getContent() << " by " << answer->getAuthor()->getName() << endl;
+                }
+                return;
+            }
+        }
+        cout << "No Question found with id: " << questionId << endl;
+    }
 };
-class Vote
+int main()
 {
-private:
-    User *voter;
-    bool isUpvote;
-public:
-};
-class Tag
-{
-private:
-    int id;
-    string name;
-public:
-};
+    Stackoverflow st;
+    User *user1 = new User(1, "luffy");
+    User *user2 = new User(2, "hyogoro");
+
+    st.postQuestion(user1, "Advanced armament haki", "how to use it , i have seen rayleigh using it.");
+    cout << "Show All Questions: " << endl;
+    st.showAllQuestions();
+    st.postAnswer(user2, 1, "just let your haki flow in your fist.");
+    st.showAnswers(1);
+}
